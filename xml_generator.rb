0c022d30bcb0a3661cbc83c86dc50c9a364c4b96
Daniel Vydra <usefulaccount@gmail.com>
2011-05-26 14:16:28 +1000

    Clean up template generator

--- xml_generator.rb
+++ xml_generator.rb
@@ -3,6 +3,12 @@ require 'json'
 require 'builder'
 require 'erb'
 
+
+def write_file filename, binding
+  template = File.read("kindle_templates/#{filename}.erb")
+  File.open("tmp/#{filename}", "w") {|f| f.puts ERB.new(template).result binding}
+end
+
 todays_date = DateTime.now.to_s
 stories = JSON.parse(File.open('tmp/stories.json').read)
 
@@ -10,15 +16,4 @@ stories.each_with_index do |story, index|
   story_template = ERB.new(File.read('kindle_templates/story.html.erb'))
   File.open("tmp/story#{index}.html", "w") {|f| f.puts story_template.result binding } 
 end
-
-opf_template = ERB.new(File.read('kindle_templates/breakfast.opf.erb'))
-File.open("tmp/breakfast.opf", "w") {|f| f.puts opf_template.result binding } 
-
-ncx_template = ERB.new(File.read('kindle_templates/breakfast.ncx.erb'))
-File.open("tmp/breakfast.ncx", "w") {|f| f.puts ncx_template.result binding }
-
-toc_template = ERB.new(File.read('kindle_templates/toc.html.erb'))
-File.open("tmp/toc.html", "w") {|f| f.puts toc_template.result binding }
-
-intro_template = ERB.new(File.read('kindle_templates/intro.html.erb'))
-File.open("tmp/intro.html", "w") {|f| f.puts intro_template.result binding }
+["breakfast.opf", "breakfast.ncx", "toc.html", "intro.html"].each { |filename| write_file filename, binding }
